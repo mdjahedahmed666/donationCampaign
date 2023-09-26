0ef5c18b9d080c3139490c164088cd75fd7465cf
Jahed <mdjahedahmed666@gmail.com>
2023-09-26 12:39:23 +0100

    donation page see all buttone  added

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -3,23 +3,27 @@ import React, { useEffect, useState } from "react";
 const Donation = () => {
   const [donatedCard, setDonatedCard] = useState([]);
   const [cardLength, setCardLength] = useState(4);
+  console.log(donatedCard);
 
   useEffect(() => {
     // Retrieve the stored card details from local storage
     const storedCard = JSON.parse(localStorage.getItem("donatedCard")) || [];
     setDonatedCard(storedCard);
   }, []);
+  
 
   return (
     <div className="container mx-auto sm:px-10 md:px-32 mt-16 gap-4 grid grid-cols-1 md:grid-cols-2">
       {donatedCard.slice(0,cardLength).map((card, index) => (
-        <div className="card card-side bg-base-100 shadow-xl">
+        <div className="card card-side shadow-xl" style={{ backgroundColor: card.card_bg }}>
           <figure>
             <img src={card.picture} alt="Movie" />
           </figure>
           <div className="card-body">
-            <p>{card.category}</p>
-            <h2 className="card-title">{card.title}</h2>
+            <div className="w-20 text-center p-1 h-8">
+            <p className="" style={{ ackgroundColor: card.category_bg }}>{card.category}</p>
+            </div>
+            <h2 className="card-title" style={{ color: card.text_color }}>{card.title}</h2>
             <p>{card.price}</p>
             <div className="card-actions">
               <button className="btn btn-primary">View Details</button>
